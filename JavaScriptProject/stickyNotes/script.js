7a420c2a47993d19e45b2d470a0b9573f8562b88
DhruvAsati <asatidhruv2806@gmail.com>
2024-07-31 19:36:16 +0530

    sound and drag and drop  updated of sticky notes

--- JavaScriptProject/stickyNotes/script.js
+++ JavaScriptProject/stickyNotes/script.js
@@ -16,15 +16,25 @@ function applyStyles(element, styles) {
 }
 
 // Usage:
+ // Button click audio
+   
+ addNotesBtn.addEventListener("click", function(e){
+    const noteSound = document.getElementById("added-sound");
+    noteSound.currentTime = 0;
+    noteSound.play();
+});
+
 addNotesBtn.addEventListener("click", function() {
+  
+
+
     if (textArea.value === "") {
         alert("Please enter a note!");
         return;
     }
-
+    
     const newNoteDiv = document.createElement("div"); // Changed "myDiv" to "div"
     newNoteDiv.textContent = textArea.value; // Set the text content of the new note div
-
     // Apply styles to the new note div
     applyStyles(newNoteDiv, {
         width: "200px",
@@ -38,12 +48,54 @@ addNotesBtn.addEventListener("click", function() {
         fontWeight: "500",
         fontSize: "1.2rem"
     });
+    //draging effect
+    newNoteDiv.draggable = true;
+
+    newNoteDiv.addEventListener("dragstart", function(e){
+         
+        // moving a full newNoteDiv from its original position
+        e.dataTransfer.setData("text/plain", newNoteDiv);
+        e.dataTransfer.setData("text/html", newNoteDiv);
+
+        newNoteDiv.addEventListener("dragend", function(e){
+            // moving a newNoteDiv back to its original position
+            notelist.appendChild(newNoteDiv);
+
+        });
+
+        newNoteDiv.addEventListener("dragenter", function(e){
+            // highlighting a newNoteDiv when dragging over another
+            e.target.style.backgroundColor = "rgba(255,255,255,0.6)";
+            newNoteDiv.style.opacity = "0.5";
+            newNoteDiv.style.zIndex = "100";
+            newNoteDiv.addEventListener("dragover", function(e){
+                e.preventDefault();
+            });
+            newNoteDiv.addEventListener("dragleave", function(e){
+                e.target.style.backgroundColor = bgcolor.value;
+                newNoteDiv.style.opacity = "1";
+                newNoteDiv.style.zIndex = "1";
+            });
+
+
+        });
+    });
+
 
     // Create a remove button for each note
     const removeNote = document.createElement("button");
     removeNote.innerHTML = "X";
+   
     removeNote.addEventListener("click", function() {
         newNoteDiv.style.display = "none";
+        
+    });
+    //remove Audio Added
+    removeNote.addEventListener("click", function() {
+        const removedSound = document.getElementById("remove-sound");
+        removedSound.currentTime = 0;
+        removedSound.play();
+
     });
     textMessage.style.display = "none";
     // Append the note and remove button to the notelist
