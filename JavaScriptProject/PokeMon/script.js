d6fb803eb90c1eaf92bf304eca66e373f987a22b
DhruvAsati <asatidhruv2806@gmail.com>
2024-08-12 19:29:45 +0530

    GeeksterPok

--- JavaScriptProject/PokeMon/script.js
+++ JavaScriptProject/PokeMon/script.js
@@ -1,73 +1,135 @@
-const apiKey = 'u2_rqCIvuRVjrzovCUqqABFQfCWT8bzyoK608bclk4U';
-const images = document.querySelectorAll('.image');
+let types = document.getElementById('types');
+let reset = document.getElementById('reset');
+let filter = document.getElementById('filter');
+let search = document.getElementById('search');
+let pika = document.getElementById('pika');
+let cont = document.getElementsByClassName('pokemon-cont')[0];
 
-fetch(`https://api.unsplash.com/photos/random?query=office&count=10&client_id=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach((photo, index) => {
-            images[index].src = photo.urls.regular;
-        });
-    });
+let cardarr = [];
 
-    let search = document.getElementById('search');
-    search.addEventListener('keyup', function(event) {
-        //adding searching by string
-        let searchValue = event.target.value.toLowerCase();
-        images.forEach(image => {
-            let imageText = image.alt.toLowerCase();
-            image.style.display = imageText.includes(searchValue)? 'block' : 'none';
-        });
-        //adding sorting by name
+let colorobj = {
+    bug: "#26de81",
+    dragon: "#ffeaa7",
+    dark: "#8A8A8A",
+    electric: "#fed330",
+    fairy: "#FF0069",
+    fighting: "#30336b",
+    fire: "#f0932b",
+    flying: "#81ecec",
+    grass: "#00b894",
+    ground: "#EFB549",
+    ghost: "#a55eea",
+    ice: "#74b9ff",
+    normal: "#95afc0",
+    poison: "#6c5ce7",
+    psychic: "#a29bfe",
+    rock: "#2d3436",
+    water: "#0190FF",
+    steel: "#71797E"
+  };
+async function fetchPokemon(i) {
+    let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    let response = await data.json();
+    return response;
+}
 
-        images.sort((a, b) => {
-            let aText = a.alt.toLowerCase();
-            let bText = b.alt.toLowerCase();
-            if (aText < bText) {
-                return -1;
-            }
-            if (aText > bText) {
-                return 1;
-            }
-            return 0;
-        });
-        images.forEach(image => image.parentNode.appendChild(image));
-        //adding pagination
-        let perPage = 5;
-        let currentPage = 1;
-        let totalPages = Math.ceil(images.length / perPage);
-        let pagination = document.getElementById('pagination');
-        pagination.innerHTML = '';
-        for (let i = 1; i <= totalPages; i++) {
-            let pageButton = document.createElement('button');
-            pageButton.textContent = i;
-            pageButton.addEventListener('click', function() {
-                currentPage = i;
-                displayImages();
-            });
-            pagination.appendChild(pageButton);
-            if (i === currentPage) {
-                pageButton.classList.add('active');
-            }
+function formCard(info) {
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <div class="card-front">
+        <div class="img">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${info.id}.svg" alt="${info.species.name}">
+        </div>    
+        <h2>${info.species.name}</h2>
+        <div class="pok-type">
+        </div>
+        <div class="abilities">
+            <div class="ab">
+                <p class="head">Attack</p>
+                <p class="type">${info.stats[1].base_stat}</p>
+            </div> 
+            <div class="ab">
+                <p class="head">Defence</p>
+                <p class="type">${info.stats[2].base_stat}</p>
+            </div> 
+            <div class="ab">
+                <p class="head">Speed</p>
+                <p class="type">${info.stats[5].base_stat}</p>
+            </div> 
+        </div>
+    </div>
+    <div class="card-back">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${info.id}.png"/>
+         <h2 class="type">${info.species.name}</h2>
+         <div class="back-info">
+            <p>${info.abilities[0].ability.name}</p>
+         </div>
+    </div>
+    `
+    let poktype = div.querySelector('.pok-type');
     
-        }
-        function displayImages() {
-            let startIndex = (currentPage - 1) * perPage;
-            let endIndex = startIndex + perPage;
-            images.forEach((image, index) => {
-                if (index >= startIndex && index < endIndex) {
-                    image.style.display = 'block';
-                } else {
-                    image.style.display = 'none';
-                }
-            });
-            let totalImages = images.length;
-            document.getElementById('total-images').textContent = `Total Images: ${totalImages}`;
-            let visibleImages = images.filter(image => image.style.display === 'block').length;
-            document.getElementById('visible-images').textContent = `Visible Images: ${visibleImages}`;
-            let currentImages = images.filter(image => image.style.display === 'block').length;
-            document.getElementById('current-images').textContent = `Current Images: ${currentImages}`;
-            let currentPage = document.querySelector('.pagination button.active');
-            currentPage.textContent = currentPage;
-            
-        }
+    info.types.forEach(type => {
+        let span = document.createElement('span');
+        span.style.backgroundColor = colorobj[type.type.name];
+        span.innerText = type.type.name;
+        div.setAttribute(`data-${type.type.name}`,type.type.name);
+        poktype.appendChild(span);
+
     });
+    cont.appendChild(div);
+}
+
+async function getData() {
+    for(let i = 1; i < 50; i++) {
+        let res = await fetchPokemon(i);
+        cardarr.push(res);
+        formCard(res);
+    }
+}
+getData();
+
+ function filterCards(type) {
+    cont.innerHTML ='';
+        cardarr.forEach((pok)=>{
+            if(pok.types.some(t=>t.type.name === type)){
+                formCard(pok);
+            }
+        });
+       
+    }
+filter.addEventListener('click', ()=>{
+    filterCards(types.value);
+});
+
+search.addEventListener('keypress', ()=>{
+    let val = search.value.toLowerCase();
+    cont.innerHTML ='';
+    cardarr.forEach((pok)=>{
+       //searching with it names
+        if(pok.species.name.toLowerCase().includes(val)){
+           formCard(pok);
+        }
+        //resetting search when deleting characters
+        if(val === ''){
+            cont.innerHTML ='';
+            cardarr.forEach((pok)=>{
+                formCard(pok);
+            });
+        }
+        //searching with abilities
+        if(pok.abilities.some(a=>a.ability.name.toLowerCase().includes(val))){
+            formCard(pok);
+        }
+        //searching with types
+        if(pok.types.some(t=>t.type.name.toLowerCase().includes(val))){
+            formCard(pok);
+        }
+        
+    });
+
+});
+
+pika.addEventListener('click', (e)=>{
+    e.preventDefault();
+})
