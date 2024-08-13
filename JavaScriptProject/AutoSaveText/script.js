38db83056c5b0bf5edb6f5cb9b9b89a21d15a463
DhruvAsati <asatidhruv2806@gmail.com>
2024-08-13 06:17:45 +0530

    Auto Save Text css updated

--- JavaScriptProject/AutoSaveText/script.js
+++ JavaScriptProject/AutoSaveText/script.js
@@ -14,3 +14,34 @@ textarea.addEventListener('input', function(){
     
 });
 
+
+const outer = document.querySelector('.outer');
+const inner = document.querySelector('.inner');
+const body = document.querySelector('body');
+const h1 = document.querySelector('h1');
+// inner.addEventListener('click', function(){
+//     inner.style.transitionDuration = '2s';
+//     inner.style.transform = 'translateX(2rem)';
+//     inner.style.transitiontimingfunction = 'linear';
+//     outer.style.backgroundColor = 'black';
+
+    
+// });
+inner.addEventListener('click', function(){
+        if(inner.style.transform === 'translateX(2rem)'){
+            inner.style.transitionDuration = '1s';
+            inner.style.transform = 'translateX(0)';
+            outer.style.backgroundColor = 'grey';
+            body.style.backgroundColor = 'white';
+            h1.style.color = 'black';
+        }
+        else{
+            inner.style.transitionDuration = '1s';
+            inner.style.transform = 'translateX(2rem)';
+            outer.style.backgroundColor = 'green';
+            body.style.backgroundColor = 'black';
+            h1.style.color = 'white';
+        }
+   
+});
+
