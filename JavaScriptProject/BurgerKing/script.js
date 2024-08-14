d1ed8274374f95f1f8c686fc16de1adb36141047
DhruvAsati <asatidhruv2806@gmail.com>
2024-08-14 20:29:51 +0530

    Js and Css updated

--- JavaScriptProject/BurgerKing/script.js
+++ JavaScriptProject/BurgerKing/script.js
@@ -9,46 +9,95 @@ let orderProducts = document.querySelector('.Order-Product');
 
 orderBtn.addEventListener('click', ()=>{
     if(burger.checked && fries.checked && drinks.checked){
-      let Product = `<img src="./Assests/allitems.png" width="150px" height="150px"/>;
+    
+      let Product = `<img src="./Assests/allitems.png" width="150px" height="150px" loading="lazy"/>;
       <p class = "orderID"><b>Order ID - </b>131</p>`;
       
+      orderProducts.innerHTML = "Thank you for placing an order with us!";
+      setTimeout(()=>{
+        orderProducts.innerHTML = 'We are preparing your order please wait...';
+      }, 6000);
+      setTimeout(() =>{
       orderProducts.innerHTML = Product;
+    },10000);
     }
     else if(burger.checked && fries.checked){
-      let Product = `<img src="./Assests/burgerandfries.png" width="150px" height="150px"/>
+    
+
+      let Product = `<img src="./Assests/burgerandfries.png" width="150px" height="150px" loading="lazy"/>
       <p class = "orderID"><b>Order ID - </b>126</p>`;
+      orderProducts.innerHTML = "Thank you for placing an order with us!";
+      setTimeout(()=>{
+        orderProducts.innerHTML = 'We are preparing your order please wait...';
+      }, 6000);
+      setTimeout(() =>{
       orderProducts.innerHTML = Product;
+    },10000);
     }
     else if(burger.checked && drinks.checked){
-      let Product = `<img src="./Assests/burgeranddrinks.png" width="150px" height="150px"/>
+    
+      let Product = `<img src="./Assests/burgeranddrinks.png" width="150px" height="150px" loading="lazy"/>
       <p class = "orderID"><b>Order ID - </b>127</p>`;
+      orderProducts.innerHTML = "Thank you for placing an order with us!";
+      setTimeout(()=>{
+        orderProducts.innerHTML = 'We are preparing your order please wait...';
+      }, 6000);
+      setTimeout(() =>{
       orderProducts.innerHTML = Product;
+    },10000);
     }
     else if(fries.checked && drinks.checked){
+    
       let Product = `
-      <img src="./Assests/friesanddrinks.png" width="150px" height="150px"/>
+      <img src="./Assests/friesanddrinks.png" width="150px" height="150px" loading="lazy"/>
       <p class = "orderID"><b>Order ID - </b>130</p>`;
+      orderProducts.innerHTML = "Thank you for placing an order with us!";
+      setTimeout(()=>{
+        orderProducts.innerHTML = 'We are preparing your order please wait...';
+      }, 6000);
+      setTimeout(() =>{
       orderProducts.innerHTML = Product;
+    },10000);
     }
     else if(fries.checked){
-      let Product = `<img src="./Assests/fries.png" width="150px" height="150px"/>
+    
+      let Product = `<img src="./Assests/fries.png" width="150px" height="150px" loading="lazy"/>
       <p class = "orderID"><b>Order ID - </b>128</p>`;
+      orderProducts.innerHTML = "Thank you for placing an order with us!";
+      setTimeout(()=>{
+        orderProducts.innerHTML = 'We are preparing your order please wait...';
+      }, 6000);
+      setTimeout(() =>{
       orderProducts.innerHTML = Product;
+    },10000);
     }
     else if(drinks.checked){
-      let Product = `<img src="./Assests/drinks.png" width="150px" height="150px"/>
+    
+      let Product = `<img src="./Assests/drinks.png" width="150px" height="150px" loading="lazy"/>
       <p class = "orderID"><b>Order ID - </b>129</p>`;
+      orderProducts.innerHTML = "Thank you for placing an order with us!";
+      setTimeout(()=>{
+        orderProducts.innerHTML = 'We are preparing your order please wait...';
+      }, 6000);
+      setTimeout(() =>{
       orderProducts.innerHTML = Product;
+    },10000);
     }
    
     else if(burger.checked){
-      let Product = `<img src="./Assests/burger.png" width="150px" height="150px"/>;
+      let Product = `<img src="./Assests/burger.png" width="150px" height="150px" loading="lazy"/>;
       <p class = "orderID"><b>Order ID - </b>131</p>`;
 
+      orderProducts.innerHTML = "Thank you for placing an order with us!";
+      setTimeout(()=>{
+        orderProducts.innerHTML = 'We are preparing your order please wait...';
+      }, 6000);
+      setTimeout(() =>{
       orderProducts.innerHTML = Product;
+    },10000);
     }
     else{
-      orderProducts.innerHTML = '';
+      alert("Please place an order first");
     }
   });
 
