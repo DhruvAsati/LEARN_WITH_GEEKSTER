ad98e0c36daebef6f800bff5e2a280de184cac34
DhruvAsati <asatidhruv2806@gmail.com>
2024-09-17 18:37:38 +0530

    React

---
+++ REACT-JS/react-vite/src/Content.jsx
@@ -0,0 +1,12 @@
+import styles from './Content.module.css';
+
+const Content = () =>{
+    return (
+        <div>
+            <h2 className = {styles.contenth2}>Content Page</h2>
+            <p>Welcome to the Content Page</p>
+            <p>This is a simple React component</p>
+        </div>
+    )
+} 
+export default Content;
---
+++ REACT-JS/react-vite/src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = ( ) => {
+    return (
+        <div>
+            <p>Created by Geekster with React</p>
+            <p>Version 1.0.0</p>
+        </div>
+    )
+}
+export default Footer;
